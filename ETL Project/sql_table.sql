c014418a78800a7497f148cdbb6e57a07fdb0b75
Jasmin Hong <jasmin@dhcp-v102-085.mobile.uci.edu>
2019-12-02 17:59:39 -0800

    Final ETL Updates

---
+++ ETL Project/sql_table.sql
@@ -0,0 +1,13 @@
+CREATE TABLE etl_project(
+	year SERIAL NOT NULL,
+   	month SERIAL NOT NULL,
+	city VARCHAR (50) NOT NULL,
+	weather_val VARCHAR (50) NOT NULL,
+	arr_flights FLOAT (50) NOT NULL,
+	arr_cancelled FLOAT (50) NOT NULL,
+	arr_delay FLOAT (50) NOT NULL,
+	carrier_delay FLOAT (50) NOT NULL,
+	weather_delay FLOAT (50) NOT NULL
+);
+
+SELECT * FROM etl_project
